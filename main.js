f8d8f31383b0c8f93092883f7b90b363d9bbd246
Shymaa kelany <kelanymmd123@gmail.com>
2021-11-06 15:59:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.item-box');
+let count = list.length;
+for(let i=0; i<count; i++){
+  list[i].addEventListener('click',function(){
+    for(let j=0; j<count; j++){
+      list[j].classList.remove('active');
+    }
+    this.classList.add('active');
+
+    let dataFilter = this.getAttribute('data-filter');
+    
+    for(let k =0; k<itemBox.length; k++){
+
+      itemBox[k].classList.remove('active');
+      itemBox[k].classList.add('hide');
+
+      if(itemBox[k].getAttribute('data-item') == dataFilter ||
+       dataFilter == "all" ){
+        itemBox[k].classList.remove('hide');
+        itemBox[k].classList.add('active');
+      }
+    }
+  })
+}
